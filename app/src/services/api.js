01985962d6277aa82f674b3c4a83b807150647b7
joao.paulo <perseu456@gmail.com>
2019-04-09 16:17:48 -0300

    structure of the project

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,8 +1,8 @@
-//Usando o axios para fazer a conexão com a API
-import axios from 'axios';
+// //Usando o axios para fazer a conexão com a API
+// import axios from 'axios';
 
-const api = axios.create({
-    baseURL: 'https://rocketseat-node.herokuapp.com/api'
-})
+// const api = axios.create({
+//     baseURL: 'https://rocketseat-node.herokuapp.com/api'
+// })
 
-export default api;
+// export default api;
